9a2e0e3f4e37d75cb89d29f61385f22502f236ac
mmparadinha <matheusmparadinha@gmail.com>
2023-04-17 19:26:02 -0300

    Add needed starters, folders and DTOs

--- src/main/java/com/tweteroo/api/APIApplication.java
+++ src/main/java/com/tweteroo/api/APIApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class APIApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(APIApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(APIApplication.class, args);
+  }
 
 }
